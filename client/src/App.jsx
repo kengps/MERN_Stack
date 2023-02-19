981f65c8768b9b7d8f3206f43188a12987243b19
kengps <106742004+kengps@users.noreply.github.com>
2023-02-19 18:56:52 +0700

    update display singleComponent

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,24 +1,21 @@
-
-import './App.css'
-import{ BrowserRouter, Route, Routes } from 'react-router-dom'
-import MyRouter from './MyRouter';
-import BlogComponent from './components/BlogComponent';
-import SingleComponent from './components/SingleComponent';
-
-
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import MyRouter from "./MyRouter";
+import BlogComponent from "./components/BlogComponent";
+import SingleComponent from "./components/SingleComponent";
 
 function App() {
-
   return (
     <BrowserRouter>
-      
       <Routes>
-          <Route path='/'  element={<MyRouter/>}/>
-          <Route path='/create'  element={<BlogComponent/>}/>
-          <Route path='/blog/:slug'  element={<SingleComponent />} />
+        <Route path="/" element={<MyRouter />} />
+        <Route path="/create" element={<BlogComponent />} />
+        <Route path="/blog/">
+          <Route path=":slug" element={<SingleComponent/>}/>
+        </Route>
       </Routes>
     </BrowserRouter>
   );
 }
 
-export default App
+export default App;
--- client/src/components/SingleComponent.jsx
+++ client/src/components/SingleComponent.jsx
@@ -1,10 +1,28 @@
-const SingleComponent = (props) => {
-    console.log(JSON.stringify(props));
-    return (
-     <>
-     The slug is 
-     </>
-    )
-  }
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
+const SingleComponent = () => {
+  const slug = useParams();
+  const props = slug.slug
+  console.log("props" + props);
+
+  const [blog, setBlog] = useState("");
+
+  useEffect(() => {
+    axios
+      .get(`${import.meta.env.VITE_REACT_APP_API}/blog/${props}`)
+      .then((response) => {
+        setBlog(response);
+        console.log("respone =", response);
+        console.log("respone 2=", response.data.title);
+        console.log("respone 3=", response.data.content);
+   
+      })
+      .catch((err) => alert(err));
+  }, []);
+  return <div>The content is {JSON.stringify(blog)}</div>;
   
-  export default SingleComponent
+};
+
+export default SingleComponent;
